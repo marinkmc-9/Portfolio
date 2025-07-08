7a6058bc3d90294c3d21c9ee7906202dd7d3c21d
marinkmc-9 <marinkmc@gmail.com>
2025-07-08 14:44:03 +0900

    Contract登録機能

--- src/main/java/co/jp/nakamichi/controller/ContractController.java
+++ src/main/java/co/jp/nakamichi/controller/ContractController.java
@@ -3,7 +3,11 @@ package co.jp.nakamichi.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import co.jp.nakamichi.entity.Contract;
 import co.jp.nakamichi.service.ContractService;
 
 @Controller
@@ -23,4 +27,23 @@ public class ContractController {
         // contract/list.htmlに画面遷移
         return "contract/list";
     }
+
+    // ----- 追加:ここから -----
+    /** Contract登録画面を表示 */
+    @GetMapping("/register")
+    public String getRegister(@ModelAttribute Contract contract) { // ---新規登録
+        // User登録画面に遷移
+        return "contract/register";
+    }
+
+    /** Contract登録処理 */
+    @PostMapping("/register")
+    public String postRegister(Contract contract) { // ---Contract contractに新規登録のデータが登録される。
+        // Contract登録（Mysqlのテーブルに保存）
+        service.saveContract(contract);
+        // 一覧画面にリダイレクト
+        return "redirect:/contract/list";
+    }
+    // ----- 追加:ここまで -----
+
 }
--- src/main/java/co/jp/nakamichi/service/ContractService.java
+++ src/main/java/co/jp/nakamichi/service/ContractService.java
@@ -2,6 +2,8 @@ package co.jp.nakamichi.service;
 
 import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional; // 追加
+
 import co.jp.nakamichi.entity.Contract;
 import co.jp.nakamichi.repository.ContractRepository;
 
@@ -18,5 +20,15 @@ public class ContractService {
         // リポジトリのfindAllメソッドを呼び出す
         return contractRepository.findAll();
     }
+
+    // ----- 追加:ここから -----
+    /** Contractの登録を行なう（Mysqlのpriceテーブルに保存） */
+    @Transactional
+    public Contract saveContract(Contract contract) {
+        return contractRepository.save(contract);
+    }
+    // ----- 追加:ここまで -----
+
 }
+
 
